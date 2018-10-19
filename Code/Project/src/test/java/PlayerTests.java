73c0ae08ca05c7acde032daff639beb539193245
Jameson Axton <Jameson.Axton@gmail.com>
2018-10-19 10:04:23 -0500

    Updated POM to include all the JUnit and Jar stuff.

--- Code/Project/src/test/java/PlayerTests.java
+++ Code/Project/src/test/java/PlayerTests.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class PlayerTests {
+
+    @ParameterizedTest
+    @DisplayName("Default Constructor")
+    @CsvSource({"name, 22"})
+    void playerConstructorTest(String name, int age) {
+
+    }
 }
+
