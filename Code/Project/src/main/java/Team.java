6bacce694b2238587c38ab2cbb1a149dda2432f5
Hunter Michalk <Hunter_Michalk@baylor.edu>
2018-10-19 13:27:29 -0500

    Changed Lists to Maps

--- Code/Project/src/main/java/Team.java
+++ Code/Project/src/main/java/Team.java
@@ -1,19 +1,69 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class Team {
-	List<Player> roster = new ArrayList<Player>();
-	List<Player> alumniRoster = new ArrayList<Player>();
-	List<User> adminPriveleges = new ArrayList<User>();
-	String teamName;
-	
-	public void addPlayer(Player p) {
-		roster.add(p);
-	}
-	
-	public Player removePlayer(Player p) {
-		if(roster.contains(p.getID())) {
-			roster.remove(p);
-		}
-	}
+    Map<Long,Player> roster = new TreeMap<Long,Player>();
+    Map<Long,Player> alumniRoster = new TreeMap<Long,Player>();
+    List<User> adminPrivileges = new ArrayList<User>();
+    String teamName;
+
+    /**
+     * 
+     * @param player p
+     */
+    public void addPlayer(Player p) {
+        Objects.requireNonNull(p);
+        
+        if( alumniRoster.contains(p.getID()) ) {
+            roster.add(p.getID() , p );            
+        }
+    }
+
+    /**
+     * 
+     * @param player p
+     * @return player p
+     */
+    public Player removePlayer(Player p) {
+        Objects.requireNonNull(p);
+
+        if (roster.contains(p)) {
+            roster.remove(p);
+            alumniRoster.add(p);
+        }
+
+        return p;
+    }
+
+    /**
+     * 
+     * @param User ad
+     */
+    public void addAdmin(User ad) {
+        Objects.requireNonNull(ad);
+
+        if (!adminPrivileges.contains(ad)) {
+            adminPrivileges.add(ad);
+        }
+    }
+    
+    /**
+     * 
+     * @param User ad
+     * @return User ad
+     */
+    public User removeAdmin(User ad) {
+        Objects.requireNonNull(ad);
+        
+        if(adminPrivileges.contains(ad)) {
+            adminPrivileges.remove(ad);
+        }
+        
+        return ad;
+    }
 }
--- Code/Project/src/test/java/TeamTester.java
+++ Code/Project/src/test/java/TeamTester.java
@@ -1,26 +1,47 @@
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 public class TeamTester {
-	protected Team theTeam = null;
-	
-	@BeforeEach
-	void init() {
-		theTeam = new Team();
-	}
-	
-	@Test
-	void addNewPlayer(Player p) {
-		theTeam.addPlayer(p);
-	}
-	
-	@Test
-	void removePlayer(Player p) {
-		theTeam.removePlayer(p);
-	}
-	
-	@Test
-	void removeNull(){
-		theTeam.removePlayer(null);
-	}
+    protected Team theTeam = null;
+
+    @BeforeEach
+    void init() {
+        theTeam = new Team();
+    }
+
+    @Test
+    void addNewPlayer(Player p) {
+        theTeam.addPlayer(p);
+    }
+
+    @Test
+    void addNullPlayer(Player p) {
+        assertThrows(NullPointerException.class, () -> {
+            theTeam.addPlayer(null);
+        });
+    }
+
+    @Test
+    void removePlayer(Player p) {
+        theTeam.removePlayer(p);
+    }
+
+    @Test
+    void removeNull() {
+        assertThrows(NullPointerException.class, () -> {
+            theTeam.removePlayer(null);
+        });
+    }
+
+    @Test
+    void addNewAdmin(User ad) {
+        theTeam.addAdmin(ad);
+    }
+    
+    @Test
+    void addNullAdmin() {
+        theTeam.addAdmin(null);
+    }
 }
