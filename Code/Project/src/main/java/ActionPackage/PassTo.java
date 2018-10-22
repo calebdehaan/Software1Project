07af3c161369d3ad65701ed687dca3f15302a098
Matt Giles <Matt_Giles@baylor.edu>
2018-10-22 16:02:04 -0500

    Fixing PassTo (I hope)

---
+++ Code/Project/src/main/java/ActionPackage/PassTo.java
@@ -0,0 +1,32 @@
+package ActionPackage;
+
+import javafx.util.Pair;
+
+public class PassTo extends Action {
+    PassTo(Long id, ActionEnum e) {
+        super(id);
+        this.description = "";
+        this.actionName = e;
+    }
+    
+    PassTo(Long id, String desc) {
+        super(id);
+        this.description = desc;
+    }
+    
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+    
+    @Override
+    public Pair<Long, ActionEnum> updateStats() {
+        return new Pair<Long, ActionEnum>(playerId, ActionEnum.PASSCOMPLETE);
+    }
+    
+    @Override
+    public void visit(ActionVisitor av) {
+        av.accept(this);
+    }
+    
+}
