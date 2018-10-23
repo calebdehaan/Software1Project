0aad3a3d1669b809afabcbdfeca7c3aef1df7985
Jameson <Jameson.Axton@gmail.com>
2018-10-22 19:02:01 -0500

    Fixed Clone in Player. Added test for it.

--- Code/Project/src/main/java/DataBase/Player.java
+++ Code/Project/src/main/java/DataBase/Player.java
@@ -4,9 +4,7 @@ import DataBase.utility.DominantHand;
 import DataBase.utility.Height;
 import DataBase.utility.Weight;
 
-import java.lang.reflect.Field;
-
-public class Player {
+public class Player implements Cloneable {
     private Height height;
     private Weight weight;
     private int age = -1;
@@ -34,24 +32,24 @@ public class Player {
     
     // Constructors //
     public Player() {
-        this(new Height(0,0), new Weight(0F), AGE_MIN, "none", DominantHand.Right);
+        this(new Height(0, 0), new Weight(0F), AGE_MIN, "none", DominantHand.Right);
     }
     
     public Player(String name) {
-        this(new Height(0,0), new Weight(0F), AGE_MIN, name, DominantHand.Right);
+        this(new Height(0, 0), new Weight(0F), AGE_MIN, name, DominantHand.Right);
     }
     
     public Player(Height height, Weight weight, int age, String name, DominantHand domHand) {
         this.setWeight(weight);
         this.setHeight(height);
         this.setAge(age);
-        this.setId(this.hashCode());
         this.setName(name);
         this.setDomHand(domHand);
+        this.setId(this.hashCode());
     }
     
     // Utility Functions //
-    public Player clone(){
+    public Player clone() {
         Player p = new Player();
         
         p.setWeight(weight);
@@ -60,19 +58,19 @@ public class Player {
         p.setId(this.getId());
         p.setName(name);
         p.setDomHand(domHand);
+        
         p.setInjured(injured);
         p.setGamesPlayed(gamesPlayed);
-        
-        for(Field f : this.getClass().getDeclaredFields()){
-        
-        }
+        p.setCompletions(completions);
+        p.setCatches(catches);
+        p.setFouls(fouls);
+        p.setInjuries(injuries);
+        p.setScores(scores);
+        p.setCompletionRate();
         
         return p;
     }
     
-    
-    // TODO: Validate input in setters
-    // Generated //
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -82,16 +80,41 @@ public class Player {
         
         if (age != player.age) return false;
         if (id != player.id) return false;
+        if (passes != player.passes) return false;
+        if (completions != player.completions) return false;
+        if (catches != player.catches) return false;
+        if (scores != player.scores) return false;
+        if (fouls != player.fouls) return false;
+        if (injuries != player.injuries) return false;
+        if (injured != player.injured) return false;
+        if (gamesPlayed != player.gamesPlayed) return false;
+        if (Double.compare(player.completionRate, completionRate) != 0) return false;
+        if (!height.equals(player.height)) return false;
+        if (!weight.equals(player.weight)) return false;
         if (!name.equals(player.name)) return false;
         return domHand == player.domHand;
     }
     
     @Override
     public int hashCode() {
-        int result = name.hashCode();
+        int result;
+        long temp;
+        result = height.hashCode();
+        result = 31 * result + weight.hashCode();
         result = 31 * result + age;
         result = 31 * result + (int) (id ^ (id >>> 32));
+        result = 31 * result + name.hashCode();
         result = 31 * result + domHand.hashCode();
+        result = 31 * result + passes;
+        result = 31 * result + completions;
+        result = 31 * result + catches;
+        result = 31 * result + scores;
+        result = 31 * result + fouls;
+        result = 31 * result + injuries;
+        result = 31 * result + (injured ? 1 : 0);
+        result = 31 * result + gamesPlayed;
+        temp = Double.doubleToLongBits(completionRate);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
         return result;
     }
     
@@ -200,31 +223,35 @@ public class Player {
     }
     
     public void incrementCatch() {
-    	this.catches++;
+        this.catches++;
     }
     
     public void incrementCompletions() {
-    	this.incrementThrows();
-    	this.setCompletions(this.getCompletions() + 1);
+        this.incrementThrows();
+        this.setCompletions(this.getCompletions() + 1);
     }
     
     public void incrementThrows() {
-    	this.passes++;
+        this.passes++;
     }
-
-	public int getCompletions() {
-		return completions;
-	}
-
-	public void setCompletions(int completions) {
-		this.completions = completions;
-	}
-
-	public double getCompletionRate() {
-		return completionRate;
-	}
-
-	public void setCompletionRate() {
-		this.completionRate = this.completions/this.catches;
-	}
+    
+    public int getCompletions() {
+        return completions;
+    }
+    
+    public void setCompletions(int completions) {
+        this.completions = completions;
+    }
+    
+    public double getCompletionRate() {
+        return completionRate;
+    }
+    
+    public void setCompletionRate() {
+        if (this.catches == 0) {
+            this.completionRate = 0;
+        } else {
+            this.completionRate = this.completions / this.catches;
+        }
+    }
 }
