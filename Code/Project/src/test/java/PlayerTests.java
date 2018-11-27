9bab774ab53e341026977110381a3deb87caf33b
Jameson Axton <Jameson.Axton@gmail.com>
2018-11-27 14:02:00 -0600

    Added helper functions to PlayerTests for generating Dynamic Tests.

--- Code/Project/src/test/java/PlayerTests.java
+++ Code/Project/src/test/java/PlayerTests.java
@@ -11,32 +11,45 @@ import java.util.List;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PlayerTests {
     private static final int NUM_TESTS = 100;
-
+    private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     private static Player validPlayer, twin1, twin2;
-    private static List<DynamicTest> validPlayers, invalidPlayers;
 
 
     @BeforeAll
     static void setup(){
         validPlayer = new Player(new Height(6, 3), new Weight(180), 22, "Tobin", DominantHand.Right);
-        validPlayers = generateValidPlayers();
-        invalidPlayers = generateInvalidPlayers();
     }
 
+    private static int rand(int lower, int upper){
+        Random random = new Random();
+        return random.nextInt((upper - lower) + 1) + lower;
+    }
+
+    private static String randomString(int length) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int index = rand(0, ALPHA_NUMERIC_STRING.length() - 1);
+            builder.append(ALPHA_NUMERIC_STRING.charAt(index));
+        }
+        return builder.toString();
+    }
+
+
     @TestFactory
-    static List<DynamicTest> generateValidPlayers(){
+    List<DynamicTest> valueConstructorTestsValid(){
         List<DynamicTest> result = new ArrayList<>(NUM_TESTS);
 
         for(int i = 0; i < NUM_TESTS; i++){
             result.add(DynamicTest.dynamicTest("Valid Player #" + i, () -> {
-                Height height = new Height((int) (Math.random() * 2), (int) (Math.random() * 2));
+                Height height = new Height(rand(0,10), rand(0,100));
                 Weight weight = new Weight((new Random()).nextDouble());
-                int age = ((new Random()).nextInt() + Player.AGE_MIN) % Player.AGE_MAX;
+                int age = rand(Player.AGE_MIN, Player.AGE_MAX);
                 String name = "Player";
-                DominantHand hand = DominantHand.values()[(new Random()).nextInt() % 2]; // TODO: 2 or 3?
+                DominantHand hand = DominantHand.values()[rand(0, 2)];
 
                 new Player(height, weight, age, name, hand);
             }));
@@ -46,18 +59,18 @@ public class PlayerTests {
     }
 
     @TestFactory
-    static List<DynamicTest> generateInvalidPlayers(){
+    List<DynamicTest> valueConstructorTestsInvalid(){
         List<DynamicTest> result = new ArrayList<>(NUM_TESTS);
 
         for(int i = 0; i < NUM_TESTS - 2; i++){
             result.add(DynamicTest.dynamicTest("Invalid Player #" + i, () -> {
-                Height height = new Height((int) (Math.random() * 2), (int) (Math.random() * 2));
+                Height height = new Height(rand(-20,20), rand(-100,100));
                 Weight weight = new Weight((new Random()).nextDouble());
                 int age = (new Random()).nextInt();
-                String name = "Player".substring((new Random()).nextInt() % "Player".length());
-                DominantHand hand = DominantHand.values()[(new Random()).nextInt() % 2]; // TODO: 2 or 3?
+                String name = randomString(rand(0, 100));
+                DominantHand hand = DominantHand.values()[rand(0,2)]; 
 
-                new Player(height, weight, age, name, hand);
+                assertThrows(Exception.class, () -> new Player(height, weight, age, name, hand));
             }));
         }
 
@@ -65,9 +78,9 @@ public class PlayerTests {
             Height height = new Height((int) (Math.random() * 2), (int) (Math.random() * 2));
             Weight weight = new Weight((new Random()).nextDouble());
             int age = (new Random()).nextInt();
-            DominantHand hand = DominantHand.values()[(new Random()).nextInt() % 2]; // TODO: 2 or 3?
+            DominantHand hand = DominantHand.values()[rand(0,2)]; 
 
-            new Player(height, weight, age, null, hand);
+            assertThrows(Exception.class, () -> new Player(height, weight, age, null, hand));
         }));
 
         result.add(DynamicTest.dynamicTest("Invalid Player #" + (NUM_TESTS - 1), () -> {
@@ -75,9 +88,9 @@ public class PlayerTests {
             Weight weight = new Weight((new Random()).nextDouble());
             int age = (new Random()).nextInt();
             String name = "";
-            DominantHand hand = DominantHand.values()[(new Random()).nextInt() % 2]; // TODO: 2 or 3?
+            DominantHand hand = DominantHand.values()[rand(0,2)]; 
 
-            new Player(height, weight, age, name, hand);
+            assertThrows(Exception.class, () -> new Player(height, weight, age, name, hand));
         }));
 
         return result;
@@ -115,19 +128,6 @@ public class PlayerTests {
                 () -> Assertions.assertThrows(IllegalArgumentException.class, () -> new Player(""))
         );
     }
-
-    @Test
-    @DisplayName("Valid Full Constructor")
-    void constructorTestValid(){
-
-    }
-
-    @Test
-    @DisplayName("Invalid Full Constructor")
-    void constructorTestInvalid(){
-
-    }
-
 
     @Test
     @DisplayName("Valid Clone")
