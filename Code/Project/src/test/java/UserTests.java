40207ae6db64e908c3d5e108dcd90f60bf5fc92a
Jameson Axton <Jameson.Axton@gmail.com>
2018-11-27 18:56:50 -0600

    Converted User to Singleton

---
+++ Code/Project/src/test/java/UserTests.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class UserTests {
+
+    @Test
+    @DisplayName("Valid Value Constructor")
+    void valueConstructorValid(){
+
+    }
+}
