ccfa649547a5845dca2a3261e0da471132cfbc8e
Matt Giles <Matt_Giles@baylor.edu>
2018-11-27 19:29:10 -0600

    Work thus far, database is connected to just working on adding queries

--- Code/Project/src/main/java/graphicalUserInterface/createNewPlayer/CreatePlayerController.java
+++ Code/Project/src/main/java/graphicalUserInterface/createNewPlayer/CreatePlayerController.java
@@ -14,11 +14,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class CreatePlayerController {
-	private static final String DB_DRIVER = "org.gjt.mm.mysql.Driver";
-	private static final String DB_CONNECTION = "jdbc:mysql://localhost/Database";
-	private static final String DB_USER = "TheUser";
-	private static final String DB_PASSWORD = "UserOnly";
-	
+	private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+	private static final String DB_CONNECTION = "jdbc:mysql://localhost/TheProjectData";
+	private static final String DB_USER = "root";
+	private static final String DB_PASSWORD = "Digby1097";
+
 	private Main main;
 
 	private ObservableList<Integer> ageBoxList = FXCollections.observableArrayList(16, 17, 18, 19, 20, 21, 22, 23);
@@ -53,20 +53,24 @@ public class CreatePlayerController {
 	private void goConfirm() throws IOException, SQLException {
 		Connection dbConnection = getDBConnection();
 		Statement statement = dbConnection.createStatement();
-		String theQuery = "Insert into Player (Height, Weight, Age, Name, Hand, Passes, Completions, Catches, Scores, Injured, GamesPlayed)" +
-		                  "Values (\'" + heightField.getText() + "\', \'" + weightField.getText() + "\', " + ageBox.getValue() + ", \'" +
-		                  nameField.getText() + "\', \'" + dominantHandBox.getValue() + "\', 0, 0, 0, 0, 0, 0";
+		String theQuery = "Insert into Player (Height, Weight, Age, Name, Hand, Passes, Completions, Catches, Scores, Injured, GamesPlayed) "
+				+ "Values (\'" + heightField.getText() + "\', \'" + weightField.getText() + "\', \'" + ageBox.getValue()
+				+ "\', \'" + nameField.getText() + "\', \'" + dominantHandBox.getValue() + "\', \'0\', \'0\', \'0\', \'0\', \'0\', \'0\');";
+		System.out.println(theQuery);
 		statement.executeQuery(theQuery);
+		System.out.println("executed");
 		main.showCreateNewPlayerVerificationScene();
 		System.out.println("Hey");
 	}
-	
+
 	private static Connection getDBConnection() {
 		Connection dbConnection = null;
 		try {
 			Class.forName(DB_DRIVER);
 		} catch (ClassNotFoundException e) {
 			System.out.println(e.getMessage());
+			System.out.println("Failed to establish database");
+			System.exit(-1);
 		}
 		try {
 			dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
