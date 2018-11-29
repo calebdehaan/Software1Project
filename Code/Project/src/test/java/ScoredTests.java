27c3d8ab41feb17f514ce1640cade8efdc9887db
Caleb DeHaan <calebdehaan@gmail.com>
2018-11-29 02:15:53 -0600

    We have edited the ppt as well as deleted a little bit of the gui

--- Code/Project/src/test/java/ScoredTests.java
+++ Code/Project/src/test/java/ScoredTests.java
@@ -4,15 +4,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 
 public class ScoredTests {
-    @Test
-    @DisplayName("Valid Value Constructor")
-    public void defaultConstructorTestValid() {
-        Assertions.assertNotNull(new Scored(0L, "none"));
-    }
+	@Test
+	@DisplayName("Valid Value Constructor")
+	public void defaultConstructorTestValid() {
+		Assertions.assertNotNull(new Scored(0L, "none"));
+	}
 
-    @Test
-    @DisplayName("Invalid Value Constructor")
-    public void defaultConstructorTestInvalid() {
-        Assertions.assertThrows(NullPointerException.class, () -> new Scored(0L, null));
-    }
+	@Test
+	@DisplayName("Invalid Value Constructor")
+	public void defaultConstructorTestInvalid() {
+		Assertions.assertThrows(NullPointerException.class, () -> new Scored(0L, null));
+	}
 }
